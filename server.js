5a76a49bb0355af77e9258c2298b2b1d987fb7a4
aero177-jpg <aeron.jahr@gmail.com>
2021-05-05 16:09:59 +0200

    fixed newBud

--- server.js
+++ server.js
@@ -1,203 +1,209 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
-const { authenticate } = require('./middleware');
+const { authenticate } = require("./middleware");
 
-const { 
-    getUsers,
-    createUser,
-    getUserByEpost
- } = require('./services/database-brukere');
+const {
+  getUsers,
+  createUser,
+  getUserByEpost,
+} = require("./services/database-brukere");
 
- const { 
-    getBudgetsByEmail,
-    createBudget,
-    deleteBudget,
-    updateBudget
- } = require('./services/database-budsjett');
+const {
+  getBudgetsByEmail,
+  createBudget,
+  deleteBudget,
+  updateBudget,
+} = require("./services/database-budsjett");
 
- const {
-    lagNyKategori,
-    slettKategori,
-    endreKategori,
-    getKatsByBudsjettID
- } = require('./services/database-kategori');
+const {
+  lagNyKategori,
+  slettKategori,
+  endreKategori,
+  getKatsByBudsjettID,
+} = require("./services/database-kategori");
 
-const { 
-    slettBudsjettpost,
-    endreBudsjettpost,
-    lagNyBudsjettpost,
-    getBudsjettposter
-} = require('./services/database-budsjettpost');
+const {
+  slettBudsjettpost,
+  endreBudsjettpost,
+  lagNyBudsjettpost,
+  getBudsjettposter,
+} = require("./services/database-budsjettpost");
 
-const port = process.env.PORT || '3001';
-const secret = 'somethingverysecret1234'
+const port = process.env.PORT || "3001";
+const secret = "somethingverysecret1234";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/bruker', async (req, res, next) => {
-    try {
-        const brukere = await getUsers();
-        res.send(brukere);
-    } catch (err) {
-        next(err);
-    }  
+app.get("/bruker", async (req, res, next) => {
+  try {
+    const brukere = await getUsers();
+    res.send(brukere);
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.post('/registrer', async (req, res) => {
-    const { navn, epost, passord } = req.body;
+app.post("/registrer", async (req, res) => {
+  const { navn, epost, passord } = req.body;
 
-    try {
-        const passordHash = await bcrypt.hash(passord, 10);
+  try {
+    const passordHash = await bcrypt.hash(passord, 10);
 
-        const bruker = await createUser(navn, epost, passordHash);
+    const bruker = await createUser(navn, epost, passordHash);
 
-        const token = jwt.sign({
-            ID: bruker.ID,
-            navn: bruker.navn,
-            epost: bruker.epost
-        }, Buffer.from(secret, 'base64'));
-
-        res.send({
-            token: token
-        });
-    } catch (error) {
-        res.send(error.message);
-    }
-})
-
-// Session
-app.post('/session', async (req, res) => {
-    const { epost, passord } = req.body;
-
-    try {
-        const bruker = await getUserByEpost(epost);
-        if(!bruker) {
-            return res.status(401).send({ error: 'ukjent bruker' });
-        }
-
-        const isCorrectPassword = await bcrypt.compare(passord, bruker.passord);
-
-        if (!isCorrectPassword) {
-            return res.status(401).send({ error: 'feil passord' });
-        }
-
-        const token = jwt.sign({
-            ID: bruker.ID,
-            epost: bruker.epost,
-            navn: bruker.navn,
-        }, Buffer.from(secret, 'base64'));
-
-        res.send({
-            token: token
-        })
-
-    } catch(error) {
-        res.status(500).send({error: error.message});
-    }
-});
-
-app.get('/session', authenticate, (req, res) => {
-    const { epost } = req.body;
+    const token = jwt.sign(
+      {
+        ID: bruker.ID,
+        navn: bruker.navn,
+        epost: bruker.epost,
+      },
+      Buffer.from(secret, "base64")
+    );
 
     res.send({
-        message: `Du er innlogget som ${epost}.`
-    })
-   
-})
+      token: token,
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+// Session
+app.post("/session", async (req, res) => {
+  const { epost, passord } = req.body;
+
+  try {
+    const bruker = await getUserByEpost(epost);
+    if (!bruker) {
+      return res.status(401).send({ error: "ukjent bruker" });
+    }
+
+    const isCorrectPassword = await bcrypt.compare(passord, bruker.passord);
+
+    if (!isCorrectPassword) {
+      return res.status(401).send({ error: "feil passord" });
+    }
+
+    const token = jwt.sign(
+      {
+        ID: bruker.ID,
+        epost: bruker.epost,
+        navn: bruker.navn,
+      },
+      Buffer.from(secret, "base64")
+    );
+
+    res.send({
+      token: token,
+    });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+app.get("/session", authenticate, (req, res) => {
+  const { epost } = req.body;
+
+  res.send({
+    message: `Du er innlogget som ${epost}.`,
+  });
+});
 
 // budsjett
-app.get('/budsjett/:epost', async (req, res) => {
-    const { epost } = req.params;
-    const budsjetter = await getBudgetsByEmail(epost);
-    res.send(budsjetter);
-  });
+app.get("/budsjett/:epost", async (req, res) => {
+  const { epost } = req.params;
+  const budsjetter = await getBudgetsByEmail(epost);
+  res.send(budsjetter);
+});
 
-app.post('/budsjett', async (req, res) => {
-    const {tittel, epostOwner, noNullEmailArray} = req.body;
-    const newBudget = await createBudget(tittel, epostOwner, noNullEmailArray);
-    res.send({newBudget});
-});  
+app.post("/budsjett", async (req, res) => {
+  const { tittel, epostOwner, noNullEmailArray } = req.body;
+  const newBudget = await createBudget(tittel, epostOwner, noNullEmailArray);
+  res.send({ newBudget });
+});
 
-app.delete('/budsjett/:budsjettID', async (req, res) => {
-    const { budsjettID } = req.params;
-    await deleteBudget(budsjettID);
-    res.send({budsjettID});
-})
+app.delete("/budsjett/:budsjettID", async (req, res) => {
+  const { budsjettID } = req.params;
+  await deleteBudget(budsjettID);
+  res.send({ budsjettID });
+});
 
-app.put('/budsjett/:ID', async (req, res) => {
-    const {tittel,ID} = req.body;
-    // const {budsjettID} = req.params;
-    const nyttBudsjett = await updateBudget(tittel, ID);
-    res.send({nyttBudsjett});
-})
-
+app.put("/budsjett/:ID", async (req, res) => {
+  const { tittel, ID } = req.body;
+  // const {budsjettID} = req.params;
+  const nyttBudsjett = await updateBudget(tittel, ID);
+  res.send({ nyttBudsjett });
+});
 
 //budsjettpost
-app.post('/budsjettpost', async (req, res) => {
-    const { tittel, sum, fast, kategoriID} = req.body;
-    const nyBudsjettpost = await lagNyBudsjettpost(tittel, sum, fast, kategoriID);
-    res.send(nyBudsjettpost);
-})
+app.post("/budsjettpost", async (req, res) => {
+  const { tittel, sum, fast, kategoriID } = req.body;
+  const nyBudsjettpost = await lagNyBudsjettpost(tittel, sum, fast, kategoriID);
+  res.send(nyBudsjettpost);
+});
 
-app.delete('/budsjettpost/:budsjettpostID', async (req, res) => {
-    const { budsjettpostID } = req.params;
-    await slettBudsjettpost(budsjettpostID);
-    res.send(budsjettpostID);
-})
+app.delete("/budsjettpost/:budsjettpostID", async (req, res) => {
+  const { budsjettpostID } = req.params;
+  await slettBudsjettpost(budsjettpostID);
+  res.send(budsjettpostID);
+});
 
-app.put('/budsjettpost/:budsjettpostID', async (req, res) => {
-    const {tittel, sum, fast, budsjettpostID} = req.body;
-    const endretBudsjettpost = await endreBudsjettpost(tittel, sum, fast, budsjettpostID);
-    res.send(endretBudsjettpost);
-})
+app.put("/budsjettpost/:budsjettpostID", async (req, res) => {
+  const { tittel, sum, fast, budsjettpostID } = req.body;
+  const endretBudsjettpost = await endreBudsjettpost(
+    tittel,
+    sum,
+    fast,
+    budsjettpostID
+  );
+  res.send(endretBudsjettpost);
+});
 
-app.get('/budsjettpost/:kategoriID', async (req, res) => {
-    const { kategoriID } = req.params;
-    const budsjettposterByKat = await getBudsjettposter(kategoriID);
-    res.send(budsjettposterByKat);
-})
-
+app.get("/budsjettpost/:kategoriID", async (req, res) => {
+  const { kategoriID } = req.params;
+  const budsjettposterByKat = await getBudsjettposter(kategoriID);
+  res.send(budsjettposterByKat);
+});
 
 // kategori
-app.post('/kategori', async (req, res) => {
-    const { tittel, budsjettID } = req.body;
-    const nyKategori = await lagNyKategori(tittel, budsjettID);
-    res.send(nyKategori);
-})
+app.post("/kategori", async (req, res) => {
+  const { tittel, budsjettID } = req.body;
+  const nyKategori = await lagNyKategori(tittel, budsjettID);
+  res.send(nyKategori);
+});
 
-app.get('/kategori/:budsjettID', async (req, res) => {
-    const { budsjettID } = req.params;
-    const kats = await getKatsByBudsjettID(budsjettID);
-    res.send(kats);
-  });
+app.get("/kategori/:budsjettID", async (req, res) => {
+  const { budsjettID } = req.params;
+  const kats = await getKatsByBudsjettID(budsjettID);
+  res.send(kats);
+});
 
-app.put('/kategori/:kategoriID', async (req, res) => {
-    const {tittel,kategoriID} = req.body;
-    const kategori = await endreKategori(tittel, kategoriID);
-    res.send({kategori});
-})
-app.put('/budsjett/:ID', async (req, res) => {
-    const {tittel,ID} = req.body;
-    // const {budsjettID} = req.params;
-    const nyttBudsjett = await updateBudget(tittel, ID);
-    res.send({nyttBudsjett});
-})
+app.put("/kategori/:kategoriID", async (req, res) => {
+  const { tittel, kategoriID } = req.body;
+  const kategori = await endreKategori(tittel, kategoriID);
+  res.send({ kategori });
+});
+app.put("/budsjett/:ID", async (req, res) => {
+  const { tittel, ID } = req.body;
+  // const {budsjettID} = req.params;
+  const nyttBudsjett = await updateBudget(tittel, ID);
+  res.send({ nyttBudsjett });
+});
 
-app.delete('/kategori/:kategoriID', async (req, res) => {
-    const { kategoriID } = req.params;
-    const kategori = await slettKategori(kategoriID);
-    res.send({kategori});
-    await slettKategori(kategoriID);
-    res.send(kategoriID);
-})
-
+app.delete("/kategori/:kategoriID", async (req, res) => {
+  const { kategoriID } = req.params;
+  const kategori = await slettKategori(kategoriID);
+  res.send({ kategori });
+  await slettKategori(kategoriID);
+  res.send(kategoriID);
+});
 
 // app.delete('/budsjett/:budsjettID', async (req, res) => {
 //     const { budsjettID } = req.params;
@@ -205,6 +211,5 @@ app.delete('/kategori/:kategoriID', async (req, res) => {
 //     res.send({budsjettID});
 // })
 app.listen(port, () => {
-    console.log(`budget API listening on ${port}`)
+  console.log(`budget API listening on ${port}`);
 });
-
--- services/database-budsjett.js
+++ services/database-budsjett.js
@@ -1,73 +1,87 @@
-var mysql2 = require('mysql2');
+var mysql2 = require("mysql2");
 
 const database = mysql2.createPool({
-    host: 'eu-cdbr-west-01.cleardb.com',
-    user: 'b2634025820b95',
-    database: 'heroku_15cdb7f59fc730e',
-    password: '581bff0c'
-})
+  host: "eu-cdbr-west-01.cleardb.com",
+  user: "b2634025820b95",
+  database: "heroku_15cdb7f59fc730e",
+  password: "581bff0c",
+});
 
 function getBudgetsByEmail(epost) {
-    return database.promise().query(`
+  return database
+    .promise()
+    .query(
+      `
     SELECT * 
         FROM budsjett
         INNER JOIN budsjett_bruker ON budsjett.ID = budsjett_bruker.budsjettID
         WHERE budsjett_bruker.epost = ?
-    `, [epost])
-      .then((result) => result[0]);
-  }
+    `,
+      [epost]
+    )
+    .then((result) => result[0]);
+}
 
-  function createRelationBudgetUser(budsjettID , epost){
-      return database.promise().query(`
+function createRelationBudgetUser(budsjettID, epost) {
+  return database.promise().query(
+    `
         INSERT INTO budsjett_bruker
             (epost, budsjettID)
         VALUES 
             (?, ?)
-      `, [
-          epost,
-          budsjettID
-      ])
-  }
-  
-  function createBudget(tittel, epostOwner, noNullEmailArray) {
-    return database.promise().query(`
+      `,
+    [epost, budsjettID]
+  );
+}
+
+function createBudget(tittel, epostOwner, noNullEmailArray) {
+  return database
+    .promise()
+    .query(
+      `
         INSERT INTO budsjett
             (tittel)
         VALUES
             (?);
-    `, [
-      tittel
-    ])
+    `,
+      [tittel]
+    )
     .then((result) => {
-      noNullEmailArray.forEach(epost => 
+      noNullEmailArray.forEach((epost) =>
         createRelationBudgetUser(result[0].insertId, epost)
-       ); 
-    }
-    )
-  }
+      );
+      return result[0].insertId;
+    });
+}
 
-  function deleteBudget(budsjettID) {
-    return database.promise().query(`
+function deleteBudget(budsjettID) {
+  return database.promise().query(
+    `
       DELETE FROM budsjett WHERE ID = ?;
-    `, 
+    `,
     [budsjettID]
-    )
-  }
+  );
+}
 
-  function updateBudget(nyTittel,budsjettID ) {
-    return database.promise().query(`
+function updateBudget(nyTittel, budsjettID) {
+  return database
+    .promise()
+    .query(
+      `
       UPDATE budsjett 
       SET
         tittel = ?
       WHERE 
         ID = ?
-    `, [nyTittel,budsjettID])
+    `,
+      [nyTittel, budsjettID]
+    )
     .then(([result]) => result[0]);
-  };
+}
 
-  module.exports = {
-    getBudgetsByEmail,
-    createBudget,
-    deleteBudget,
-    updateBudget
+module.exports = {
+  getBudgetsByEmail,
+  createBudget,
+  deleteBudget,
+  updateBudget,
 };
